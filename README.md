30ab94e168562ab781e7e22366a4caf98dc48a14
David Batey <davidlbatey@gmail.com>
2013-03-23 01:01:45 +0000

    Added best in place coffe & JS

---
+++ README.md
@@ -0,0 +1,17 @@
+# Best In Place
+
+Extends best in place inline editing to allow for date and time picker.
+
+## Requirements
+
+- best_in_place
+- jquery-ui
+- jquery-ui-timepicker-addon
+
+## Usage
+
+Use the same as the bip date type but set it to datetime
+
+```ruby
+= best_in_place object, :attribute, :path => object_path(object), :type => :datetime
+```
---
+++ best_in_place_datetime.js
@@ -0,0 +1,49 @@
+(function() {
+  var datetime;
+
+  datetime = {
+    "datetime": {
+      activateForm: function() {
+        var input_elt, output, that;
+        that = this;
+        output = jQuery(document.createElement('form')).addClass('form_in_place').attr('action', 'javascript:void(0);').attr('style', 'display:inline');
+        input_elt = jQuery(document.createElement('input')).attr('type', 'text').attr('name', this.attributeName).attr('value', this.sanitizeValue(this.display_value));
+        if (this.inner_class !== null) {
+          input_elt.addClass(this.inner_class);
+        }
+        output.append(input_elt);
+        this.element.html(output);
+        this.setHtmlAttributes();
+        this.element.find('input')[0].select();
+        this.element.find("form").bind('submit', {
+          editor: this
+        }, BestInPlaceEditor.forms.input.submitHandler);
+        this.element.find("input").bind('keyup', {
+          editor: this
+        }, BestInPlaceEditor.forms.input.keyupHandler);
+        return this.element.find('input').datetimepicker({
+          dateFormat: "yy-mm-dd",
+          timeFormat: "HH:mm:ss",
+          parse: 'loose',
+          onClose: function() {
+            return that.update();
+          }
+        }).datepicker('show');
+      },
+      getValue: function() {
+        return this.sanitizeValue(this.element.find("input").val());
+      },
+      submitHandler: function(event) {
+        return event.data.editor.update();
+      },
+      keyupHandler: function(event) {
+        if (event.keyCode === 27) {
+          return event.data.editor.abort();
+        }
+      }
+    }
+  };
+
+  $.extend(BestInPlaceEditor.forms, datetime);
+
+}).call(this);
